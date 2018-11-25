9816ba2f909adcef2e8be5d728b5aedbd28bea21
Clement18Z <44888538+Clement18Z@users.noreply.github.com>
2018-11-25 21:19:47 +0800

    ADDSTAR

---
+++ 1122_PHP_practice/TESTSTAR/star.php
@@ -0,0 +1,23 @@
+<center>
+<?php
+	$x=11;
+	$x=($x/2)-0.5;
+ 	// echo $x;
+	for($i=-$x;$i<=$x;$i++){
+		 echo "*";
+		for($j=0;$j<abs($i);$j++){
+			echo "*";
+		}
+		echo "<br>";
+	}
+	
+	
+	
+?>
+
+
+
+</center>	
+
+
+
